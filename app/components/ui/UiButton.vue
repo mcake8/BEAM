28d581db721419180af9f13202d417c960f28e5e
Vladislav Iampolskii <cakefronend@gmail.com>
2026-05-03 19:01:49 +0400

    test button

---
+++ app/components/ui/UiButton.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <button class="ui-button">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'button'
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
