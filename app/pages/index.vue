2e4c7214047d2680f5fe54b4cc5261f1aa84b19a
artuha62 <hellrain.art@gmail.com>
2026-05-05 14:51:42 +0300

    feat: project setup - nuxt, scss, vitest, embla carousel, css grid

---
+++ app/pages/index.vue
@@ -0,0 +1,79 @@
+<script setup>
+import useEmblaCarousel from 'embla-carousel-vue'
+
+const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+
+const goToPrev = () => emblaApi.value?.scrollPrev()
+const goToNext = () => emblaApi.value?.scrollNext()
+</script>
+
+<template>
+	<div class="container">
+		<div class="content">
+			<div class="product">
+				<h1 class="title">Люнн</h1>
+				<p class="description">Стул Пашки Дурова</p>
+
+				<div class="slider">
+					<div
+						ref="emblaRef"
+						class="slider__viewport"
+					>
+						<div class="slider__container">
+							<div
+								v-for="n in 3"
+								:key="n"
+								class="slide"
+							>
+								<img
+									src="/images/chair-1.png"
+									alt=""
+								/>
+								Slide {{ n }}
+							</div>
+						</div>
+					</div>
+
+					<button @click="goToPrev">←</button>
+					<button @click="goToNext">→</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+.content {
+	border-radius: var(--border-radius-small);
+	background-color: #fff;
+	display: flex;
+	min-height: 100dvh;
+}
+
+.product {
+	display: flex;
+	flex-direction: column;
+}
+
+.slider {
+	&__viewport {
+		overflow: hidden;
+	}
+
+	&__container {
+		display: flex;
+		touch-action: pan-y pinch-zoom;
+	}
+}
+
+.slide {
+	flex: 0 0 100%;
+	min-width: 0;
+	height: 300px;
+	background: #eee;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	user-select: none;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: false },
+
+	modules: ['@nuxt/eslint'],
+
+	css: [
+		'~/assets/styles/normalize.css',
+		'~/assets/styles/fonts.scss',
+		'~/assets/styles/global.scss',
+		'~/assets/styles/utils.scss',
+		'~/assets/styles/variables.scss',
+		'~/assets/styles/grid.scss'
+	],
+
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "~/assets/styles/helpers/index.scss" as *;'
+				}
+			}
+		}
+	}
 })
